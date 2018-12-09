6a5a42f596a9989a86d1934a96ce8ce7ecac6410
Karen Li <kli19@stuy.edu>
2018-12-09 16:05:16 -0500

    can change data

--- shmegegge.c
+++ shmegegge.c
@@ -24,10 +24,23 @@ int main(){
     printf("You've just created a shared memory segment!\n");
   }
   else{
-    printf("This is the data in this shared segment: %s\n", data);
+    printf("This is the data in this shared memory segment: %s\n", data);
   }
 
-  printf("Do you want to change the data in this shared segment?\n");
+  printf("Do you want to change the data in this shared memory segment? (y/n):\n");
+  char line[100];
+  fgets(line, 100, stdin);
+  if (line[0] == 'y'){
+    printf("Enter the new data:\n");
+    fgets(line, 100, stdin);
+    strcpy(data,line);
+    printf("The data in the shared memory segment is now: %s\n", data);
+  }
+  else if(*line == 'n'){
+    printf("Understood! No changes to the data in this shared memory segment.\n");
+  }
+  shmdt(data);
   
+
   return 0;
 }
